e268f4503c56acb4f8588851e7edc458994eac00
CyberTitus <82715493+CyberTitus@users.noreply.github.com>
2022-06-13 17:14:46 +0100

    Commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-
+# edited
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
